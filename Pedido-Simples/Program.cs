18819b1e59116fd1aaa3101307f825f83eec9f91
Gabriel Vasconcellos <gavapim@gmail.com>
2019-12-29 11:27:51 -0300

    Primeiro commit com os arquivos iniciais

---
+++ Pedido-Simples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pedido_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
