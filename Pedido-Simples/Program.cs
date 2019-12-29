f8c10c28fda33a87ffbbb39a8e60b2cd434480d0
Gabriel Vasconcellos <gavapim@gmail.com>
2019-12-29 15:58:32 -0300

    Sistema finalizado

--- Pedido-Simples/Program.cs
+++ Pedido-Simples/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using Pedido_Simples.Entities;
 
 namespace Pedido_Simples
 {
@@ -6,7 +8,42 @@ namespace Pedido_Simples
     {
         static void Main(string[] args)
         {
-            
+            // Entrada dos dados do Cliente
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("E-mail: ");
+            string clientEmail = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items do this order? ");
+            int items = int.Parse(Console.ReadLine());
+
+            Client client = new Client(clientName, clientEmail, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            for(int i = 1; i <= items; i++)
+            {
+                Console.WriteLine("Enter #" + i + " item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+                OrderItem item = new OrderItem(quantity, productPrice, product);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
         }
     }
 }
