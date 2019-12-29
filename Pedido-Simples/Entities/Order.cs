ddcf3c8bcde5de411142789c4ee9c624c8e13896
Gabriel Vasconcellos <gavapim@gmail.com>
2019-12-29 13:04:59 -0300

    Atualizacoes no Teste

--- Pedido-Simples/Entities/Order.cs
+++ Pedido-Simples/Entities/Order.cs
@@ -6,7 +6,20 @@ namespace Pedido_Simples.Entities
     {
         public DateTime Moment { get; set; }
         public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
         public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
 
         public void AddItem(OrderItem item)
         {
@@ -18,8 +31,16 @@ namespace Pedido_Simples.Entities
             Items.Remove(item);
         }
 
-        public Order()
+        public double Total()
         {
+            double sum = 0.0;
+
+            foreach(OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+
+            return sum;
         }
     }
 }
